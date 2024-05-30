b532c49b06618051b7c5e3f37a9531e922953551
rihapfirdaus <muhammadrihap448@gmail.com>
2024-05-30 23:11:29 +0700

    refactor: add action

--- app/src/main/java/com/capbatu/workify/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/capbatu/workify/ui/welcome/WelcomeActivity.kt
@@ -2,10 +2,13 @@ package com.capbatu.workify.ui.welcome
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.capbatu.workify.databinding.ActivityWelcomeBinding
+import com.capbatu.workify.ui.login.LoginActivity
+import com.capbatu.workify.ui.register.RegisterActivity
 
 class WelcomeActivity : AppCompatActivity() {
     private val binding: ActivityWelcomeBinding by lazy {
@@ -17,6 +20,23 @@ class WelcomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         playAnimation()
+        setupAction()
+    }
+
+    private fun setupAction() {
+        binding.apply {
+            btnRegister.setOnClickListener {
+                Intent(this@WelcomeActivity, RegisterActivity::class.java).also { intent ->
+                    startActivity(intent)
+                }
+            }
+
+            btnLogin.setOnClickListener {
+                Intent(this@WelcomeActivity, LoginActivity::class.java).also { intent ->
+                    startActivity(intent)
+                }
+            }
+        }
     }
 
     private fun playAnimation() {
@@ -24,11 +44,10 @@ class WelcomeActivity : AppCompatActivity() {
             val lottie = ObjectAnimator.ofFloat(lottieMeditation, View.ALPHA, 1f).setDuration(3000)
             val tvTitle = ObjectAnimator.ofFloat(tvAppTitle, View.ALPHA, 1f).setDuration(2000)
             val tvDesc = ObjectAnimator.ofFloat(tvAppDesc, View.ALPHA, 1f).setDuration(2000)
-            val btnLogin = ObjectAnimator.ofFloat(btnLogin, View.ALPHA, 1f).setDuration(1000)
-            val btnRegister = ObjectAnimator.ofFloat(btnRegister, View.ALPHA, 1f).setDuration(1000)
+            val llAction = ObjectAnimator.ofFloat(llAppAction, View.ALPHA, 1f).setDuration(1000)
 
             AnimatorSet().apply {
-                playSequentially(lottie, tvTitle, tvDesc, btnLogin, btnRegister)
+                playSequentially(lottie, tvTitle, tvDesc, llAction)
                 start()
             }
         }
