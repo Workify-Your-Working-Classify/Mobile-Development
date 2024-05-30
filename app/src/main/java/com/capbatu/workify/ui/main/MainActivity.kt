4785e7665f8c865e97ac028c9eb5a5f0d3bd5a13
rihapfirdaus <muhammadrihap448@gmail.com>
2024-05-30 20:22:54 +0700

    perf: implement view binding

---
+++ app/src/main/java/com/capbatu/workify/ui/main/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.capbatu.workify.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.capbatu.workify.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
